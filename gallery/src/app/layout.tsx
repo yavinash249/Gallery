8972a247d3cb3d44122186a76aeb60dfc182e615
Avinash Yadav <95971529+yavinash249@users.noreply.github.com>
2024-02-24 17:22:15 +0530

    1st commit

---
+++ gallery/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import Navbar from './components/Navbar'
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const revalidate = 10
+
+export const metadata: Metadata = {
+  title: 'Next.js Image Gallery',
+  description: 'Net Ninja tutorial series by Dave Gray',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+    <Navbar/>
+        <main className="max-w-6xl mx-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
